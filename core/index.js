0f13d187cbe3a00b35a251f6b43d0bebf4f3fe65
Valery Kiseliou <v.a.kiselyov@gmail.com>
2019-08-29 22:09:47 +0300

    Init.

--- core/index.js
+++ core/index.js
@@ -3,5 +3,5 @@
  * @property {Object} config
  * @property {Object} db
  */
-export { config } from './../config'
-export { db }  from './mysql'
+export { config } from '../config'
+export { db }  from './mysql/index'
